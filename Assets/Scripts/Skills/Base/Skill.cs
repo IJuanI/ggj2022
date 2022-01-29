d438f2c436d4f2df66b8e5099400d726305f1c68
IJuanI <juancojcl@hotmail.com>
2022-01-29 12:07:52 -0300

    feat: Skills cooldown

--- Assets/Scripts/Skills/Base/Skill.cs
+++ Assets/Scripts/Skills/Base/Skill.cs
@@ -3,17 +3,29 @@ using UnityEngine.InputSystem;
 
 public abstract class Skill : MonoBehaviour {
 
+    public float cooldown = 0f;
+    float lastCastTime = -Mathf.Infinity;
+
+    void OnValidate()
+    {
+        if (cooldown < 0)
+            cooldown = 0;
+    }
+
     public Transform caster { protected get; set; }
 
     public void TryCast(InputAction action) {
         if (CanCast(action)) {
             Cast();
+            lastCastTime = Time.time;
         }
     }
 
     public virtual void Init() { }
 
-    public virtual bool CanCast(InputAction action) { return true; }
+    public virtual bool CanCast(InputAction action) {
+        return Time.time - lastCastTime > cooldown;
+    }
 
     public abstract void Cast();
 
